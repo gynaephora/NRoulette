98d9d00e031af77dc2ad5f4de612acb53a5d2af8
Volodymyr Korzhovskyy <it.mustango@gmail.com>
2016-09-30 11:43:31 +0300

    First Activity

---
+++ app/src/main/java/com/apps/mustango/nroulette/SavedMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.apps.mustango.nroulette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_movies);
+    }
+}
