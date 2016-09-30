e71312563d068bdcc88bde4b34b8decbf88f80c4
Volodymyr Korzhovskiy <it.mustango@gmail.com>
2016-09-30 17:18:32 +0300

    add HttpQuery class

---
+++ app/src/main/java/com/apps/mustango/nroulette/httpQuery/httpQuery.java
@@ -0,0 +1,42 @@
+package com.apps.mustango.nroulette.httpQuery;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Volodymyr Korzhovskyi on 30.09.2016.
+ */
+public class httpQuery {
+
+    byte[] getUrlBytes(String urlSpec) throws IOException{
+        URL url=new URL(urlSpec);
+        HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+
+        try{
+            ByteArrayOutputStream out=new ByteArrayOutputStream();
+            InputStream in=connection.getInputStream();
+            if (connection.getResponseCode()!=HttpURLConnection.HTTP_OK){
+                return null;
+            }
+
+            int bytesRead=0;
+            byte[] buffer=new byte[1024];
+            while ((bytesRead=in.read(buffer))>0){
+                out.write(buffer,0,bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        }finally {
+         connection.disconnect();
+        }
+    }
+
+    public String getUrl(String urlSpec)throws IOException{
+        return new String(getUrlBytes((urlSpec)));
+    }
+
+}
